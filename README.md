b777ef477e8e65de73b191d2d35eaebe2fc36310
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-11-01 08:25:53 +0530

    HTML with node

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Personal-Portfolio-with-node
-Personal-Portfolio-with node
+# Personal-Portfolio-Website
+<p> This portfolio website showcases my past projects and other personal information, as well as highlights the potential I have to learn and grow in the industry.. </p>
---
+++ my-portfolio-backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Endpoint to serve CV file
+app.get('/download-cv', (req, res) => {
+  const filePath = path.join(__dirname, 'CV', 'Nimeth-Fernando-CV.pdf');
+  res.download(filePath, 'Nimeth_Fernando_CV.pdf');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost: ${PORT}`);
+});
